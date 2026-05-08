28a8f9fd4839f0ae9b5452edac40410d277cc649
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-08 13:33:34 +0300

    step 9 last

--- main.js
+++ main.js
@@ -56,24 +56,22 @@ form.addEventListener("submit", (event) => {
   const name = usernameInput.value.trim();
   const age = Number(ageInput.value);
   result.textContent = `Имя: ${name}, Возраст: ${age}`;
+
+  if (name === "") {
+    result.textContent = "Ошибка: Имя не может быть пустым!";
+    result.style.color = "red";
+    usernameInput.focus();
+    return;
+  }
+
+  if (isNaN(age) || age <= 0 || age > 120) {
+    result.textContent = "Ошибка: Введите корректный возраст (от 1 до 120)!";
+    result.style.color = "red";
+    ageInput.focus();
+    ageInput.value = "";
+    return;
+  }
+  result.textContent = `Данные сохранены: ${name}, ${age} лет`;
+  result.style.color = "green";
+  form.reset();
 });
-
-if (name === "") {
-  result.textContent = "Ошибка: Имя не может быть пустым!";
-  result.style.color = "red";
-  usernameInput.focus();
-  return;
-}
-
-if (isNaN(age) || age <= 0 || age > 120) {
-  result.textContent = "Ошибка: Введите корректный возраст (от 1 до 120)!";
-  result.style.color = "red";
-  ageInput.focus();
-  ageInput.value = "";
-
-  return;
-}
-result.textContent = `Данные сохранены: ${name}, ${age} лет`;
-result.style.color = "green";
-
-form.reset();
